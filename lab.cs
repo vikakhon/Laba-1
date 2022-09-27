5c160ba239c1689bed73265423926b9824b12e1d
Vikakhon <vikusya200206@gmail.com>
2022-09-27 11:55:02 +0300

    Fibonacci

--- lab.cs
+++ lab.cs
@@ -1,1 +1,28 @@
-decimal;
+using System;
+namespace Fib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trip = 0;
+            int next = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(next);
+                if (next == 0)
+                {
+                    next = 1;
+                }
+                else
+                {
+                    int a = next;
+                    next = next + trip;
+                    trip = a;
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
